e060baf71a659606016f4004b9a1beb1097f02de
Yipeng Sun <syp@umd.edu>
2021-06-29 17:27:03 +0200

    WIP #2: Define overlay in a more stupid way and try again.

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,15 +1,13 @@
 final: prev:
 
 {
-  darwin = prev.darwin // {
-    apple_sdk_10_12 = prev.callPackage ./apple-sdk {
-      inherit (prev.buildPackages.darwin) print-reexports;
-      inherit (prev.darwin) darwin-stubs;
-    };
+  apple_sdk_10_12 = prev.callPackage ./apple-sdk {
+    inherit (prev.buildPackages.darwin) print-reexports;
+    inherit (prev.darwin) darwin-stubs;
   };
   root = prev.callPackage ./root {
     python = final.python3;
-    inherit (final.darwin.apple_sdk.frameworks) Cocoa CoreSymbolication OpenGL;
+    inherit (final.apple_sdk_10_12.frameworks) Cocoa CoreSymbolication OpenGL;
     noSplash = true;
   };
 }
