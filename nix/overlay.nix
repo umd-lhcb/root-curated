10963e6dae8bf7e2ad3fec175f5d80e0bd00555c
Yipeng Sun <syp@umd.edu>
2021-06-29 16:21:05 +0200

    WIP #2: Trying to override part of Apple's framework.

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,9 +1,15 @@
 final: prev:
 
 {
+  darwin = darwin.override {
+    apple_sdk_10_12 = prev.callPackage ./apple-sdk {
+      inherit (prev.buildPackages.darwin) print-reexports;
+      inherit (prev.darwin) darwin-stubs;
+    };
+  };
   root = prev.callPackage ./root {
     python = final.python3;
-    inherit (final.darwin.apple_sdk.frameworks) Cocoa OpenGL;
+    inherit (final.darwin.apple_sdk.frameworks) Cocoa CoreSymbolication OpenGL;
     noSplash = true;
   };
 }
