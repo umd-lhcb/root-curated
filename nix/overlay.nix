228fc30b5f3bd9c4c57a010c33ad5a6891e821ba
Yipeng Sun <syp@umd.edu>
2021-06-29 17:22:44 +0200

    Edit typo.

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,7 +1,7 @@
 final: prev:
 
 {
-  darwin = darwin // {
+  darwin = prev.darwin // {
     apple_sdk_10_12 = prev.callPackage ./apple-sdk {
       inherit (prev.buildPackages.darwin) print-reexports;
       inherit (prev.darwin) darwin-stubs;
