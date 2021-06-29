cdcfd358478d369ce67060f2d50e6ab3a2426075
Yipeng Sun <syp@umd.edu>
2021-06-29 16:24:50 +0200

    WIP #2: Maybe this is the right way to override darwin?

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,7 +1,7 @@
 final: prev:
 
 {
-  darwin = darwin.override {
+  darwin = darwin // {
     apple_sdk_10_12 = prev.callPackage ./apple-sdk {
       inherit (prev.buildPackages.darwin) print-reexports;
       inherit (prev.darwin) darwin-stubs;
