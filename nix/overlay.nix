2fc15d7a21f57508a978a4d8f80b45c97176e33a
Yipeng Sun <syp@umd.edu>
2021-07-03 20:20:27 +0200

    Use older toolchains for ROOT 5.

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -15,6 +15,7 @@ final: prev:
   };
   root_5_34_38 = prev.callPackage ./root_5_34 {
     inherit (prev.darwin.apple_sdk.frameworks) Cocoa OpenGL;
+    stdenv = if prev.stdenv.cc.isClang then prev.llvmPackages_5.stdenv else prev.gcc8Stdenv;
     noSplash = true;
   };
 }
