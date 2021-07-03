f399b7a47e28b6cf0f0e557eddd2c61a3ae7d2fe
Yipeng Sun <syp@umd.edu>
2021-07-03 02:46:55 +0200

    Add patched ROOT 5.34.38.

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -6,4 +6,8 @@ final: prev:
     inherit (prev.darwin.apple_sdk.frameworks) Cocoa CoreSymbolication OpenGL;
     noSplash = true;
   };
+  root_5_34_38 = prev.callPackage ./root_5_34 {
+    inherit (prev.darwin.apple_sdk.frameworks) Cocoa CoreSymbolication OpenGL;
+    noSplash = true;
+  };
 }
