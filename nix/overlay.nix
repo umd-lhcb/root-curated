7cbf1d1c2d5e8d7d2f3b29a69c32c88c39c1612d
Yipeng Sun <syp@umd.edu>
2021-07-03 02:40:08 +0200

    WIP #2: Update to latest nixpkgs-unstable so that ROOT build by default.

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,13 +1,9 @@
 final: prev:
 
 {
-  apple_sdk_10_12 = prev.callPackage ./apple-sdk {
-    inherit (prev.buildPackages.darwin) print-reexports;
-    inherit (prev.darwin) darwin-stubs;
-  };
   root = prev.callPackage ./root {
     python = final.python3;
-    inherit (final.apple_sdk_10_12.frameworks) Cocoa CoreSymbolication OpenGL;
+    inherit (prev.darwin.apple_sdk.frameworks) Cocoa CoreSymbolication OpenGL;
     noSplash = true;
   };
 }
