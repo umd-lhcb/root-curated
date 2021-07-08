1d5ccb77d3e23d012e8ed9575e600a9d054a5b69
Yipeng Sun <syp@umd.edu>
2021-07-08 02:13:13 +0200

    WIP #7: Disable ROOT 6.12 since it doesn't build on Big Sur.

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -12,12 +12,12 @@ final: prev:
     inherit (prev.darwin.apple_sdk.frameworks) Cocoa CoreSymbolication OpenGL;
     noSplash = true;
   };
-  # ROOT 6.12 doesn't support Python 3 :-(
-  root_6_12_06 = prev.callPackage ./root_6_12 {
-    python = final.python2;
-    inherit (prev.darwin.apple_sdk.frameworks) Cocoa CoreSymbolication OpenGL;
-    noSplash = true;
-  };
+  # ROOT 6.12 doesn't support Python 3 and doesn't build on Big Sur :-(
+  # root_6_12_06 = prev.callPackage ./root_6_12 {
+  #   python = final.python2;
+  #   inherit (prev.darwin.apple_sdk.frameworks) Cocoa CoreSymbolication OpenGL;
+  #   noSplash = true;
+  # };
   root_5_34_38 = prev.callPackage ./root_5_34 {
     inherit (prev.darwin.apple_sdk.frameworks) Cocoa OpenGL;
     stdenv = if prev.stdenv.cc.isClang then prev.llvmPackages_5.stdenv else prev.gcc8Stdenv;
