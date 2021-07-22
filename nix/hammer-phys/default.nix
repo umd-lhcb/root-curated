786c8e404cc89e9fd6edaf99831c8195c9081d74
Yipeng Sun <syp@umd.edu>
2021-07-22 20:21:44 +0200

    WIP #1: Migrate HAMMER package here.

---
+++ nix/hammer-phys/default.nix
@@ -0,0 +1,49 @@
+{ stdenv
+, cmake
+, boost
+, libyamlcpp
+, root
+, python
+, fetchFromGitLab
+}:
+
+stdenv.mkDerivation rec {
+  pname = "hammer-phys";
+  version = "1.1.0";
+
+  src = fetchFromGitLab {
+    owner = "mpapucci";
+    repo = "Hammer";
+    rev = "v${version}";
+    sha256 = "0ldf7h6capzbwigzqdklm9wrglrli5byhsla8x79vnq7v63xx332";
+  };
+
+  nativeBuildInputs = [ cmake python.pkgs.cython ];
+  buildInputs = [
+    boost
+    libyamlcpp
+    root
+    python.pkgs.numpy
+    python.pkgs.matplotlib
+  ];
+
+  patches = [ ./add_missing_header.patch ];
+
+  cmakeFlags = [
+    "-DCMAKE_INSTALL_LIBDIR=lib"
+    "-DCMAKE_INSTALL_INCLUDEDIR=include"
+    "-DBUILD_SHARED_LIBS=ON"
+    "-DWITH_PYTHON=OFF"
+    "-DWITH_ROOT=ON"
+    "-DINSTALL_EXTERNAL_DEPENDENCIES=OFF"
+  ];
+
+  # Move the .so files to the lib folder so the output looks like this:
+  #   lib/*.so
+  # instead of:
+  #   lib/Hammer/*.so
+  postFixup = ''
+    mv $out/lib/Hammer/* $out/lib
+    rm -rf $out/lib/Hammer
+  '';
+}
